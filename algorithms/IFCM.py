93b707a415c627424aa7c0dfb78dfaa3c3bb4461
Vitor Manoel <vitorearthur15@hotmail.com>
2025-09-10 04:24:07 -0300

    renaming interval-valued and radial-valued algorithms file; adding code for radial-valued algorithm; adding placeholder for radial normalization

--- algorithms/IFCM.py
+++ algorithms/IFCM.py
@@ -430,6 +430,337 @@ class AIFCM_ER:
                 print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, MANTEVE")
             else:
                 print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, AUMENTOU")
+                
+class AIFCM_ER:
+
+    def __init__(self):
+        self.prototype_vector = []
+        self.membership_matrix = []
+        self.data = []
+        self.cluster_num = 0
+        self.adequacy_history = [] # Saves the history of the adequacy criterion over the steps
+        self.T_u = 1
+
+    def objective_function(self):
+
+        value = 0
+
+        #distance component
+        lower_components = ((self.data[:,np.newaxis,:,0]-self.prototype_vector[np.newaxis,:,:,0])**2)*self.lower_weight_matrix[np.newaxis,:,:]
+        upper_components = ((self.data[:,np.newaxis,:,1]-self.prototype_vector[np.newaxis,:,:,1])**2)*self.upper_weight_matrix[np.newaxis,:,:]
+        value += np.sum(self.membership_matrix * np.sum(lower_components + upper_components, axis=2))
+
+        #entropy component
+        not_zero = self.membership_matrix > 0
+        value += self.T_u*np.sum(self.membership_matrix[not_zero]*np.log(self.membership_matrix[not_zero])) #xlogx is approx 0 at x=0
+
+        return value
+    
+    def minimum_centroid_distance(self):
+        
+        lower_components = ((self.prototype_vector[:,np.newaxis,:,0]-self.prototype_vector[np.newaxis,:,:,0])**2)*self.lower_weight_matrix[np.newaxis,:,:]
+        upper_components = ((self.prototype_vector[:,np.newaxis,:,1]-self.prototype_vector[np.newaxis,:,:,1])**2)*self.upper_weight_matrix[np.newaxis,:,:]
+        distances = np.sum(lower_components + upper_components, axis=2)
+        np.fill_diagonal(distances, np.inf)
+        
+        return np.min(distances)
+
+    def run(self, data, cluster_num, T_u=1, max_iterations=200, threshold=1e-10):
+        random.seed()
+
+        # Initialization
+
+        self.data = data
+        self.cluster_num = cluster_num
+        feature_num = data.shape[1] # Supposes the dataset is set up correctly
+        data_num = data.shape[0]
+        self.prototype_vector = []
+        self.upper_weight_matrix = np.ones((cluster_num,feature_num)) #[[1/p for row in range(feature_num)] for col in range(cluster_num)]
+        self.lower_weight_matrix = np.ones((cluster_num,feature_num))
+        self.membership_matrix = np.zeros((data_num, cluster_num))#[[0 for col in range(cluster_num)] for row in range(data_num)]
+        self.adequacy_history = []
+        self.T_u = T_u
+
+        t = 0
+
+        starting_prototypes = random.sample(range(data_num), cluster_num)
+        self.prototype_vector = data[starting_prototypes].copy()
+
+        starting_membership = random.choices(range(cluster_num), k=data_num)
+        for i, k in enumerate(starting_membership):
+            self.membership_matrix[i][k] = 1
+
+        self.adequacy_history.append(self.objective_function()) # initial value of J
+
+        while(t < max_iterations):
+
+            t = t + 1
+
+            #fuzzy cluster prototype computation (representation step)
+
+            weight_sums = np.sum(self.membership_matrix, axis=0) 
+            valid_clusters = weight_sums > 1e-10  # check zero division
+            if np.any(valid_clusters): # check if not empty
+                for bound in range(2):
+                    weighted_sum = self.membership_matrix.T @ data[:, :, bound] 
+                    self.prototype_vector[valid_clusters, :, bound] = weighted_sum[valid_clusters] / weight_sums[valid_clusters, np.newaxis]
+                
+            #width parameter computation (weighting step)
+            
+            lower_components = np.sum(((self.data[:,np.newaxis,:,0]-self.prototype_vector[np.newaxis,:,:,0])**2)*self.membership_matrix[:,:,np.newaxis], axis=0)
+            upper_components = np.sum(((self.data[:,np.newaxis,:,1]-self.prototype_vector[np.newaxis,:,:,1])**2)*self.membership_matrix[:,:,np.newaxis], axis=0)
+            numerator = (np.prod(lower_components, axis=1)*np.prod(upper_components, axis=1))**(1/(2*feature_num))
+
+            np.divide(numerator[:, np.newaxis], lower_components, out=self.lower_weight_matrix, where=lower_components > 1e-10)
+            np.divide(numerator[:, np.newaxis], upper_components, out=self.upper_weight_matrix, where=upper_components > 1e-10)
+            
+            #membership degree computation (allocation step)
+            
+            lower_components = ((self.data[:,np.newaxis,:,0]-self.prototype_vector[np.newaxis,:,:,0])**2)*self.lower_weight_matrix[np.newaxis,:,:]
+            upper_components = ((self.data[:,np.newaxis,:,1]-self.prototype_vector[np.newaxis,:,:,1])**2)*self.upper_weight_matrix[np.newaxis,:,:]
+            distance = np.sum(lower_components + upper_components, axis=2)
+            
+            intermediate = np.exp(-distance/self.T_u)
+            intermediate_sum = np.sum(intermediate, axis=1)
+            valid_data = intermediate_sum > 1e-10
+            
+            self.membership_matrix[valid_data] = intermediate[valid_data]/intermediate_sum[valid_data,np.newaxis]
+
+            #history + stop condition
+
+            self.adequacy_history.append(self.objective_function())
+
+            if(abs(self.adequacy_history[-1]-self.adequacy_history[-2]) < threshold):
+                break
+
+        return self
+
+    def run_many(self, data, cluster_num, T_u=1, max_iterations=50, reinitializations = 30, threshold=1e-5):
+        best_run = self.run(data, cluster_num, T_u, max_iterations, threshold)
+        while(reinitializations > 0):
+            trial_run = self.run(data, cluster_num, T_u, max_iterations, threshold)
+            if(best_run.adequacy_history[-1] > trial_run.adequacy_history[-1]): best_run = trial_run
+            reinitializations = reinitializations - 1
+
+        return best_run
+
+    def grid_search(self, data, cluster_num, max_iterations=50, reinitializations=30, search=[1e-2,100,1e-2], threshold=1e-5):
+        start, finish, jump = search
+        best_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+        while(start < finish):
+            start += jump
+            trial_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+            if(trial_run.minimum_centroid_distance() <= 0.1): 
+                start += jump
+                best_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+                break
+
+        return best_run
+
+    def crispy_membership(self):
+        '''
+        Converts the fuzzy membership matrix U into a discrete one
+        '''
+        hard_membership_matrix = np.zeros((len(self.data), self.cluster_num))
+
+        max_index = np.argmax(self.membership_matrix, axis=1)
+
+        hard_membership_matrix[np.arange(len(max_index)), max_index] = 1
+
+        return hard_membership_matrix
+
+    def confusion_matrix(self, true_classes):
+
+        true_classes_index = np.unique(true_classes)
+
+        pred = true_classes_index[self.predict()]
+        cm = confusion_matrix(true_classes, pred, labels=true_classes_index)
+
+        return cm
+
+    def predict(self):
+        prediction = np.argmax(self.membership_matrix, axis=1)
+        return prediction
+
+    def analyze(self):
+        print(f"J INICIAL: {self.adequacy_history[0]}")
+        for k in range(1,len(self.adequacy_history)):
+            if(self.adequacy_history[k-1]>self.adequacy_history[k]):
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, DIMINUIU")
+            elif(self.adequacy_history[k-1]==self.adequacy_history[k]):
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, MANTEVE")
+            else:
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, AUMENTOU")
+
+class ARFCM_ER:
+
+    def __init__(self):
+        self.prototype_vector = []
+        self.membership_matrix = []
+        self.data = []
+        self.cluster_num = 0
+        self.adequacy_history = []  # Saves the history of the adequacy criterion over the steps
+        self.T_u = 1
+
+    def objective_function(self):
+
+        value = 0
+
+        # distance component
+        c_diff = self.data[:, np.newaxis, :, 0] - self.prototype_vector[np.newaxis, :, :, 0]
+        r_diff = self.data[:, np.newaxis, :, 1] - self.prototype_vector[np.newaxis, :, :, 1]
+
+        lower_components = ((c_diff - r_diff) ** 2) * self.lower_weight_matrix[np.newaxis, :, :]
+        upper_components = ((c_diff + r_diff) ** 2) * self.upper_weight_matrix[np.newaxis, :, :]
+
+        value += np.sum(self.membership_matrix * np.sum(lower_components + upper_components, axis=2))
+
+        # entropy component
+        not_zero = self.membership_matrix > 0
+        value += self.T_u * np.sum(self.membership_matrix[not_zero] * np.log(self.membership_matrix[not_zero]))
+
+        return value
+    
+    def minimum_centroid_distance(self):
+
+        c_diff = self.prototype_vector[:, np.newaxis, :, 0] - self.prototype_vector[np.newaxis, :, :, 0]
+        r_diff = self.prototype_vector[:, np.newaxis, :, 1] - self.prototype_vector[np.newaxis, :, :, 1]
+
+        lower_components = ((c_diff - r_diff) ** 2) * self.lower_weight_matrix[np.newaxis, :, :]
+        upper_components = ((c_diff + r_diff) ** 2) * self.upper_weight_matrix[np.newaxis, :, :]
+
+        distances = np.sum(lower_components + upper_components, axis=2)
+        np.fill_diagonal(distances, np.inf)
+
+        return np.min(distances)
+
+    def run(self, data, cluster_num, T_u=1, max_iterations=200, threshold=1e-10):
+        random.seed()
+
+        # Initialization
+        self.data = data
+        self.cluster_num = cluster_num
+        feature_num = data.shape[1]  # Supposes the dataset is set up correctly
+        data_num = data.shape[0]
+        self.prototype_vector = []
+        self.upper_weight_matrix = np.ones((cluster_num, feature_num))
+        self.lower_weight_matrix = np.ones((cluster_num, feature_num))
+        self.membership_matrix = np.zeros((data_num, cluster_num))
+        self.adequacy_history = []
+        self.T_u = T_u
+
+        t = 0
+
+        starting_prototypes = random.sample(range(data_num), cluster_num)
+        self.prototype_vector = data[starting_prototypes].copy()
+
+        starting_membership = random.choices(range(cluster_num), k=data_num)
+        for i, k in enumerate(starting_membership):
+            self.membership_matrix[i][k] = 1
+
+        self.adequacy_history.append(self.objective_function())  # initial value of J
+
+        while t < max_iterations:
+
+            t = t + 1
+
+            # fuzzy cluster prototype computation (representation step)
+            weight_sums = np.sum(self.membership_matrix, axis=0)
+            valid_clusters = weight_sums > 1e-10
+            if np.any(valid_clusters):
+                weighted_sum_c = self.membership_matrix.T @ data[:, :, 0]
+                weighted_sum_r = self.membership_matrix.T @ data[:, :, 1]
+
+                self.prototype_vector[valid_clusters, :, 0] = weighted_sum_c[valid_clusters] / weight_sums[valid_clusters, np.newaxis]
+                self.prototype_vector[valid_clusters, :, 1] = weighted_sum_r[valid_clusters] / weight_sums[valid_clusters, np.newaxis]
+
+            # width parameter computation (weighting step)
+            c_diff = self.data[:, np.newaxis, :, 0] - self.prototype_vector[np.newaxis, :, :, 0]
+            r_diff = self.data[:, np.newaxis, :, 1] - self.prototype_vector[np.newaxis, :, :, 1]
+
+            lower_components = np.sum(((c_diff - r_diff) ** 2) * self.membership_matrix[:, :, np.newaxis], axis=0)
+            upper_components = np.sum(((c_diff + r_diff) ** 2) * self.membership_matrix[:, :, np.newaxis], axis=0)
+
+            numerator = (np.prod(lower_components, axis=1) * np.prod(upper_components, axis=1)) ** (1 / (2 * feature_num))
+
+            np.divide(numerator[:, np.newaxis], lower_components, out=self.lower_weight_matrix, where=lower_components > 1e-10)
+            np.divide(numerator[:, np.newaxis], upper_components, out=self.upper_weight_matrix, where=upper_components > 1e-10)
+
+            # membership degree computation (allocation step)
+            c_diff = self.data[:, np.newaxis, :, 0] - self.prototype_vector[np.newaxis, :, :, 0]
+            r_diff = self.data[:, np.newaxis, :, 1] - self.prototype_vector[np.newaxis :, :, 1]
+
+            lower_components = ((c_diff - r_diff) ** 2) * self.lower_weight_matrix[np.newaxis, :, :]
+            upper_components = ((c_diff + r_diff) ** 2) * self.upper_weight_matrix[np.newaxis, :, :]
+
+            distance = np.sum(lower_components + upper_components, axis=2)
+
+            intermediate = np.exp(-distance / self.T_u)
+            intermediate_sum = np.sum(intermediate, axis=1)
+            valid_data = intermediate_sum > 1e-10
+
+            self.membership_matrix[valid_data] = intermediate[valid_data] / intermediate_sum[valid_data, np.newaxis]
+
+            # history + stop condition
+            self.adequacy_history.append(self.objective_function())
+
+            if abs(self.adequacy_history[-1] - self.adequacy_history[-2]) < threshold:
+                break
+
+        return self
+
+    def run_many(self, data, cluster_num, T_u=1, max_iterations=50, reinitializations=30, threshold=1e-5):
+        best_run = self.run(data, cluster_num, T_u, max_iterations, threshold)
+        while reinitializations > 0:
+            trial_run = self.run(data, cluster_num, T_u, max_iterations, threshold)
+            if best_run.adequacy_history[-1] > trial_run.adequacy_history[-1]:
+                best_run = trial_run
+            reinitializations = reinitializations - 1
+
+        return best_run
+
+    def grid_search(self, data, cluster_num, max_iterations=50, reinitializations=30, search=[1e-2, 100, 1e-2], threshold=1e-5):
+        start, finish, jump = search
+        best_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+        while start < finish:
+            start += jump
+            trial_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+            if trial_run.minimum_centroid_distance() <= 0.1:
+                start += jump
+                best_run = self.run_many(data, cluster_num, start, max_iterations, reinitializations, threshold)
+                break
+
+        return best_run
+
+    def crispy_membership(self):
+        """
+        Converts the fuzzy membership matrix U into a discrete one
+        """
+        hard_membership_matrix = np.zeros((len(self.data), self.cluster_num))
+        max_index = np.argmax(self.membership_matrix, axis=1)
+        hard_membership_matrix[np.arange(len(max_index)), max_index] = 1
+        return hard_membership_matrix
+
+    def confusion_matrix(self, true_classes):
+        true_classes_index = np.unique(true_classes)
+        pred = true_classes_index[self.predict()]
+        cm = confusion_matrix(true_classes, pred, labels=true_classes_index)
+        return cm
+
+    def predict(self):
+        prediction = np.argmax(self.membership_matrix, axis=1)
+        return prediction
+
+    def analyze(self):
+        print(f"J INICIAL: {self.adequacy_history[0]}")
+        for k in range(1, len(self.adequacy_history)):
+            if self.adequacy_history[k - 1] > self.adequacy_history[k]:
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, DIMINUIU")
+            elif self.adequacy_history[k - 1] == self.adequacy_history[k]:
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, MANTEVE")
+            else:
+                print(f"ITERAÇÃO {k} - J: {self.adequacy_history[k-1]} -> {self.adequacy_history[k]}, AUMENTOU")
+
 
 def loadintervaldotmat(filename):
     mat = scipy.io.loadmat(filename)
@@ -448,10 +779,11 @@ def loadintervaldotmat(filename):
 def main():
     
     data, classes = loadintervaldotmat('../datasets/Horses.mat')
+    data = interval_to_radial(data)
     data = normalize_interval(data)
     
     class_labels = np.unique(classes)
-    model = AIFCM_ER()
+    model = ARFCM_ER()
     model = model.grid_search(data, len(class_labels), search=[1e-2,100,1e-2])
 
     model.analyze()
--- algorithms/utils/preprocessing.py
+++ algorithms/utils/preprocessing.py
@@ -40,3 +40,69 @@ def normalize_interval(data, method='dispersion'):
         normalized_data = (data - means) / np.sqrt(dispersions)[np.newaxis,:,np.newaxis]
     
     return normalized_data
+
+def normalize_radial(data, method='dispersion'): # currently equivalent to normalize_radial
+    """
+    Normalizes radial-valued data based on the chosen method.
+    
+    Parameters
+    ----------
+    data : ndarray
+        Radial-valued data.
+
+    method : str, default='dispersion'
+        Normalization method to apply. Supported methods include:
+        - 'dispersion': Normalizes data to have zero mean and unit dispersion per feature.
+
+    Returns
+    -------
+    normalized_data : ndarray
+        Normalized interval-valued data with the same shape as the input.
+
+    Raises
+    ------
+    ValueError
+        If the input shape is not adequate or normalization causes a division by zero.
+    """
+
+    data = np.asarray(data)
+    
+    if data.ndim != 3 or data.shape[2] != 2:
+        raise ValueError(f"Input data must be radial-valued")
+
+    means = np.mean(data, axis=0)
+    dispersions = np.sum((data-means)**2, axis=0)
+    
+    if np.any(dispersions == 0):
+            raise ValueError("Zero division")
+
+    normalized_data = (data - means) / np.sqrt(dispersions)[np.newaxis,:,np.newaxis]
+
+    return normalized_data
+
+def interval_to_radial(data):
+    """
+    Converts interval data into center-radius form.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Input array in interval format
+
+    Returns
+    -------
+    np.ndarray
+        Output array in center-radius format
+        
+    """
+    
+    data = np.asarray(data)
+    
+    lower = data[:, :, 0]
+    upper = data[:, :, 1]
+
+    center = (lower + upper) / 2
+    radius = (upper - lower) / 2
+
+    return np.stack((center, radius), axis=2)
+    
